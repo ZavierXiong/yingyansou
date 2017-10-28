29294a36204b393d8f4cdc9df625636d2e3cad51
ZavierXiong <414106349@qq.com>
2017-10-28 09:06:39 +0800

    ..

--- js/index.js
+++ js/index.js
@@ -1,5 +1,31 @@
+var url = "http://115.29.230.132:86"
+var searchType = 1;
+var country = "null";
+var myTime=setInterval("timeAdd()",1000);
 $(function(){
+    console.log(countryJson)
+    $.ajax({
+        type:"get",
+        xhrFields: { withCredentials: true },
+        url:url+"/yys/sightseer",
+        success:function(data){
+        }
+    });
     //点击切换搜索内容
+    function appendCountry(){
+        var allCountry = countryJson.dyz.concat(countryJson.fz).concat(countryJson.mz).concat(countryJson.oz).concat(countryJson.yz);
+        console.log(allCountry);
+        var selectBox = "<option value='null'>通用引擎</option>"
+        for(var i= 0,max=allCountry.length;i<max;i++){
+            var optionName = allCountry[i].typename;
+            var optionId = allCountry[i].id;
+            var optionCode = allCountry[i].code;
+            var countryLi = `<option value="${optionCode}">${optionName}</option>`;
+            selectBox+=countryLi;
+        }
+        $('#countryName').html(selectBox);
+    };
+    appendCountry();
     (function(){
         var li = $('.search-tip li');
         for(var i=0;i<li.length;i++){
@@ -11,23 +37,30 @@ $(function(){
                     var myText = $('#search-text');
                     switch(i){
                         case 0:
+                            searchType = 1;
+                            $('.placeholder').text('小提示：当选择国家引擎时，输入本土语言关键词搜索效果更佳');
                             myText.attr('placeholder','小提示：当选择国家引擎时，输入本土语言关键词搜索效果更佳');
                             lang.stop(true,false).fadeIn(400);
                             break;
                         case 1:
-                            myText.attr('placeholder','1232');
+                            searchType = 3;
+                            $('.placeholder').text('请输入要搜索的邮箱');
+                            myText.attr('placeholder','请输入要搜索的邮箱');
                             lang.stop(true,false).fadeOut(400);
                             break;
                         case 2:
-                            myText.attr('placeholder','请输入3');
+                            $('.placeholder').text('请输入要验证的邮箱');
+                            myText.attr('placeholder','请输入要验证的邮箱');
                             lang.stop(true,false).fadeOut(400);
                             break;
                         case 3:
-                            myText.attr('placeholder','请输入4');
+                            $('.placeholder').text('请输入要搜索的文档信息');
+                            myText.attr('placeholder','请输入要搜索的文档信息');
                             lang.stop(true,false).fadeOut(400);
                             break;
                         case 4:
-                            myText.attr('placeholder','请输入5');
+                            $('.placeholder').text('请输入要搜索的图片信息');
+                            myText.attr('placeholder','请输入要搜索的图片信息');
                             lang.stop(true,false).fadeOut(400);
                             break;
                         default:
@@ -37,150 +70,92 @@ $(function(){
             })(i)
         }
     })();
-    (function(){
-        function tabChange(name,clas){
-            for(var i= 0,max = name.length;i<max;i++){
-                (function(i){
-                    name[i].addEventListener('click',function(){
-                        name.removeClass(clas);
-                        $(this).addClass(clas);
-                        if(i===0){
-                            $('.sign-in-btn').hide();
-                            $('.log-in-btn').show();
-                            $('.code-box').hide();
+
+//主页搜索
+//鹰眼一下
+//    $(function(){
+        function searchIndex(){
+            var searchWord = $('#search-text').val().replace(/^(\s|\u00A0)+/,'').replace(/(\s|\u00A0)+$/,'');//去掉首尾空格
+            country = $('#countryName option:selected').val();
+            if(searchType===3){
+                second = "00";
+                minute = "00";
+                window.clearInterval(myTime);
+                myTime=setInterval("timeAdd()",1000);
+                $('.loading-layer').fadeIn();
+                $.ajax({
+                    type:"post",
+                    url:url+"/yys/search",
+                    xhrFields: { withCredentials: true },
+                    data:{
+                        "searchType":searchType,
+                        "searchWord":searchWord,
+                        "currentPage":1
+                    },
+                    success:function(data){
+                        $('.loading-layer').fadeOut();
+                        if(data.code===200){
+                            $('.client-info').fadeIn();
                         }else{
-                            $('.log-in-btn').hide();
-                            $('.sign-in-btn').show();
-                            $('.code-box').show();
+                            if(data.code===103){
+                                $('.emailBody').html('');
+                                var pageSize = data.data.page.pageSize;//不同权限下所能展示的邮箱数量
+                                var myEmail = data.data.result.emails;//所有邮箱
+                                for(var i= 0,max=pageSize;i<max;i++){
+                                    var emailUrl = myEmail[i].email;
+                                    var emailTr = `
+                                        <tr>
+                                            <td><a href="${emailUrl}" class="info-email">${emailUrl}</a></td>
+                                            <td><a href="#" class="info-vertify">验证</a></td>
+                                            <td><a href="#" class="info-contact">立刻联系</a></td>
+                                        </tr>`
+                                    $('.emailBody').append(emailTr);
+                                }
+                                $('#lostNum').html(myEmail.length-pageSize);
+                                $('.client-info').fadeIn();
+                                //var t = confirm(data.msg);
+                                //if(t){
+                                //    location.href="buy.html"
+                                //}else{
+                                //
+                                //}
+                            }
                         }
-                    })
-                })(i)
-            }
-        }
-
-        //关闭登录注册弹框
-        function close(){
-            $('#close-sign').on('click',function(){
-                $('.sign-in,.dialog-mask').fadeOut(200);
-                $('.password-warn').hide();
-                $('.user-warn').hide();
-                $('#user-tel').val("");
-                $('#user-key').val("");
-            })
-        }
-
-        function logIn(){
-            $('#log-in').on('click',function(){
-                $('.sign-in,.dialog-mask').fadeIn(200);
-                $('.sign-in-btn').hide();
-                $('.log-in-btn').show();
-                $('.code-box').hide();
-                $('.log-in-title').addClass('selected');
-                $('.sign-in-title').removeClass('selected');
-            })
-        }
-        function signIn(){
-            $('#sign-in').on('click',function(){
-                $('.sign-in,.dialog-mask').fadeIn(200);
-                $('.log-in-btn').hide();
-                $('.sign-in-btn').show();
-                $('.code-box').show();
-                $('.sign-in-title').addClass('selected');
-                $('.log-in-title').removeClass('selected');
-            })
-        }
-
-        function logInTo(){
-            $('.log-in-btn').on('click',function(){
-                if($('#user-tel').val().length!=11||!(/^1[3|4|5|8][0-9]\d{4,8}$/.test($('#user-tel').val()))){
-                    $('.user-warn').show();
-                }else if($('#user-key').val()===""||$('#user-key').val().length<6||$('#user-key').val().length>20){
-                    $('.user-warn').hide();
-                    $('.password-warn').show();
-                }else{
-                    $('.password-warn').hide();
-                    $('.user-warn').hide();
-                    $('.loading').show();//出现加载界面
-                }
-            });
-            $('#user-tel').on('blur',function(){
-                if($('#user-tel').val().length!=11||!(/^1[3|4|5|8][0-9]\d{4,8}$/.test($('#user-tel').val()))){
-                    $('.user-warn').show();
-                }else{
-                    $('.user-warn').hide();
-                }
-            });
-            $('#user-key').on('blur',function(){
-                if($('#user-key').val().length<6||$('#user-key').val().length>20){
-                    $('.password-warn').show();
-                }else{
-                    $('.password-warn').hide();
-                }
-            })
-        }
-        function signInTo(){
-            $('.sign-in-btn').on('click',function(){
-                if($('#user-tel').val().length!=11||!(/^1[3|4|5|8][0-9]\d{4,8}$/.test($('#user-tel').val()))){
-                    $('.user-warn').show();
-                    $('.password-warn').hide();
-                }else if($('#user-key').val()===""||$('#user-key').val().length<6||$('#user-key').val().length>20){
-                    $('.user-warn').hide();
-                    $('.password-warn').show();
-                }else{
-                    $('.password-warn').hide();
-                    $('.user-warn').hide();
-                    $('.loading').show();//出现加载界面
-                }
-            });
-            $('#user-tel').on('blur',function(){
-                if($('#user-tel').val().length!=11||!(/^1[3|4|5|8][0-9]\d{4,8}$/.test($('#user-tel').val()))){
-                    $('.user-warn').show();
-                }else{
-                    $('.user-warn').hide();
-                }
-            });
-            $('#user-key').on('blur',function(){
-                if($('#user-key').val().length<6||$('#user-key').val().length>20){
-                    $('.password-warn').show();
-                }else{
-                    $('.password-warn').hide();
-                }
-            })
-        }
-
-        //验证码定时器
-        function sendCode(){
-            $('#code').on('click',function(){
-                if($('#user-tel').val().length!=11||!(/^1[3|4|5|8][0-9]\d{4,8}$/.test($('#user-tel').val()))){
-                    $('.user-warn').show();
-                }else{
-                    restCode();
-                }
-            })
-            function restCode(){
-                $("#code").hide();
-                $("#disable-code").show();
-                $("#code-date").html('5');
-                var second = 5;
-                var timer = null;
-                timer = setInterval(function(){
-                    second -=1;
-                    if(second>0){
-                        $('#code-date').html(second);
-                    }else{
-                        clearInterval(timer);
-                        $("#code").show();
-                        $("#disable-code").hide();
                     }
-                },1000)
+                });
+            }else{
+                $('.yys-loading').show();
+                if(searchWord!=""){
+                    $.ajax({
+                        type:"post",
+                        url:url+"/yys/search",
+                        xhrFields: { withCredentials: true },
+                        data: {
+                            searchType: searchType,
+                            searchWord: searchWord,
+                            country: country,
+                            currentPage: 1
+                        },
+                        success:function(){
+                            $('.yys-loading').hide();
+                            location.href = "result.html?"+"searchType="+searchType+"&"+"searchWord="+searchWord+"&"+"country="+country;
+                        }
+                    });
+                }else{
+                    location.href = "index.html"
+                }
             }
         }
-        tabChange($('.tab-title span'),'selected');
-        close();
-        logIn();
-        signIn();
-        logInTo();//点击登录按钮
-        signInTo();//点击注册的按钮
-        sendCode();
-    })();
+        $(document).on('click','#searchBtnIndex',function(){
+            searchIndex();
+        });
+        $('#search-text').on("focus",function(){
+            document.onkeydown = function(e){
+                var ev = document.all ? window.event : e;
+                if(ev.keyCode==13) {
+                    searchIndex();
+                }
+            }
+        })
+    //});
 })
--- js/result.js
+++ js/result.js
@@ -1,5 +1,52 @@
+var country = "null";
+var searchType = 1;
+var searchWord = "";
+var searchTime = "";
+var currentPage = 1;
 $(function(){
+    var localTime = "";
+    function loadSearch(){
+        var loc = location.href;
+        var n1 = loc.indexOf('=');
+        var pos = new Array();
+        var n2 = loc.lastIndexOf('=');
+        pos.push(n1);
+        while(n1<n2){
+            n1 = loc.indexOf('=',n1+1);
+            pos.push(n1)
+        }
+        var s1 = loc.indexOf('&searchWord');
+        var s2 = loc.indexOf('&country');
+        if(s1>-1) {
+            searchType = loc.slice(pos[0] + 1, s1);
+            searchWord = loc.slice(pos[1] + 1, s2);
+            country = loc.slice(pos[2] + 1);
+            $('#searchInput').val(searchWord);
+            search();
+        }
+    }
+    loadSearch();//从地址栏拿主页传来的数据
+    function countryCheck(myCountry,id){
+        var myCountry = countryJson[myCountry];
+        var list = "";
+        for(var i = 0,max=myCountry.length;i<max;i++){
+            var code = myCountry[i].code;
+            var typename = myCountry[i].typename;
+            var yzList = `
+            <li>
+                <a class="typeslect" href="javascript:void(0);" cid="${code}">${typename}</a>
+            </li>`;
+            list+=yzList;
+        }
+        $(id).html(list);
+    };
+    countryCheck('yz','#drop-menu-yz');
+    countryCheck('oz','#drop-menu-oz');
+    countryCheck('mz','#drop-menu-mz');
+    countryCheck('dyz','#drop-menu-dyz');
+    countryCheck('fz','#drop-menu-fz');
     (function(){
+        //鹰学堂tab下拉效果
         function childShow(menu,child,allChild){
             var f;
             $(menu).on('click',function(){
@@ -20,6 +67,7 @@ $(function(){
         }
         childShow('#eagle-class','.more-li','#eagle-class *')
     })();
+
     (function(){
         //点击展开条件下拉框
         function showList(){
@@ -30,7 +78,7 @@ $(function(){
                         var child = $(this).children('.dropdown-menu')
                         if(child.is(':hidden')){
                             child.stop(true,false).show(200);
-                            $(this).siblings('.tip-li').children('.dropdown-menu').hide(200);
+                            $(this).siblings('.tip-li').children('.dropdown-menu').stop(true,false).hide(200);
                         }else{
                             child.stop(true,false).hide(200);
                         }
@@ -46,6 +94,17 @@ $(function(){
                     })
                 })(i)
             }
+            $('.dropdown-menu li').on('click',function(e){
+                country = $(this).children('a').attr("cid");
+                search()
+            });//点击国家下拉栏筛选
+            $('#normalCountry').on('click',function(){
+                $('.tip-li').removeClass('active');
+                $(this).addClass('active');
+                $('.dropdown-menu').stop(true,false).hide(200);
+                country='null';
+                search();
+            });//点击通用引擎
         };
         //点击切换选中以及选择条件
         function checkList(){
@@ -71,31 +130,241 @@ $(function(){
         checkList();
     })();
 
+    //分页操作
     $('#myPage').jqPaginator({
         totalPages: 100,
-        visiblePages: 10,
+        visiblePages: 5,
         currentPage: 1,
-        pagesize:5,
+        pagesize:10,
         first: '<li class="first"><a href="javascript:void(0);">首页</a></li>',
         prev: '<li class="prev"><a href="javascript:void(0);">上一页</a></li>',
         next: '<li class="next"><a href="javascript:void(0);">下一页</a></li>',
         last: '<li class="last"><a href="javascript:void(0);">最后一页</a></li>',
         page: '<li class="page"><a href="javascript:void(0);">{{page}}</a></li>',
         onPageChange: function (num, type) {
-            $('#text').html('当前第' + num + '页');
+            if(type=='change'){
+                currentPage = num;
+                search();
+            }
         }
     });
-    $('#translate').on('click',function(){
-        var appid='2015063000000001';
-        var q='myapple';
-        var salt='1435660288'
-        $.get('http://fanyi-api.baidu.com/api/trans/vip/translate',{q:q,from:'en',to:'zh',appid:appid,
-            salt:salt,sign:appid+q+salt+"12345"},function(data){
-            console.log('译文'+data)
-        })
-    })
-})
+    //鹰眼搜信息弹框tab切换
+    //(function(){
+    //    var li = $('#left-nav>li');
+    //    var content = $('.info-cnt');
+    //    for(var i = 0,max=li.length;i<max;i++){
+    //        (function(i){
+    //            li[i].addEventListener('click',function(){
+    //                li.removeClass('info-active');
+    //                $(this).addClass('info-active');
+    //                content.hide().eq(li.index(this)).show();
+    //            });
+    //        })(i);
+    //    }
+    //    $('.close-btn').on('click',function(){
+    //        $('.client-info').fadeOut();
+    //    });
+    //})();
+    //时间戳转换成日期
+    function getLocalTime(nS) {
+        return new Date(parseInt(nS)).toLocaleString().replace(/:\d{1,2}$/,' ');
+    }
+    //提取联系信息
+    $(document).on('click','.getInfo-box',function(){
+        var companyId = $(this).attr('data-id');
+        //clearInterval(myTime);
+        second = "00";
+        minute = "00";
+        myTime=setInterval("timeAdd()",1000);
+        $('.loading-layer').fadeIn();
+        $.ajax({
+            type:"post",
+            url:url+"/yys/extract",
+            xhrFields: { withCredentials: true },
+            data:{
+                "companyId":companyId
+            },
+            success:function(data){
+                closeLoad();
+                if(data.code===200){
+                    $('.emailBody').html('');
+                    var pageSize = data.data.page.pageSize;//不同权限下所能展示的邮箱数量
+                    var myEmail = data.data.result.emails;//所有邮箱
+                    var companyName = data.data.result.company_name===null?"未知":data.data.result.company_name;
+                    var location_country = data.data.result.location_country===null?"未知":data.data.result.location_country;
+                    var contact_phone = data.data.result.contact_phone===""?"无":data.data.result.contact_phone;
+                    var Facebook = data.data.result.facebook===""?"未知":data.data.result.facebook;
+                    var Linkedin = data.data.result.linkedin===""?"未知":data.data.result.linkedin;
+                    var Twitter = data.data.result.twitter===""?"未知":data.data.result.twitter;
+                    var Google = data.data.result.google===null?"未知":data.data.result.google;
+                    var Youtobe = data.data.result.youtube===""?"未知":data.data.result.youtube;
+                    var Pintertst = data.data.result.pintertst===""?"未知":data.data.result.pintertst;
+                    var doTime  = data.data.result.doTime;
+                    var searchDate = getLocalTime(doTime[doTime.length-1]);
+                    for(var i= 0,max=10;i<max;i++){
+                        var emailUrl = myEmail[i].email;
+                        var emailTr = `
+                        <tr>
+                            <td><a href="${emailUrl}" class="info-email">${emailUrl}</a></td>
+                            <td><a href="#" class="info-vertify">验证</a></td>
+                            <td><a href="#" class="info-contact">立刻联系</a></td>
+                        </tr>
+                        `
+                        $('.emailBody').append(emailTr);
+                    }
+                    $('#lostNum').html(myEmail.length-pageSize);
+                    $('#company_name').html(companyName);
+                    $('#location_country').html(location_country);
+                    $('#contact_phone').html(contact_phone);
+                    $('#doTime').html(searchDate);
+                    $('#Facebook').html(Facebook);
+                    $('#Linkedin').html(Linkedin);
+                    $('#Twitter').html(Twitter);
+                    $('#Google').html(Google);
+                    $('#Youtobe').html(Youtobe);
+                    $('#Pintertst').html(Pintertst);
+                    $('.client-info').fadeIn();
+                }else{
+                    alert(data.msg)
+                }
+            }
+        });
+    });
 
+    //搜索触发的事件
+    function search(){
+        searchTime = new Date();
+        searchWord = $('#searchInput').val().replace(/^(\s|\u00A0)+/,'').replace(/(\s|\u00A0)+$/,'');
+        if($('#searchInput').val()===""){
+            self.location="result.html"
+        }else{
+            if(localTime!=""){
+                var interval = new Date() - localTime;//当前时间减上一次点击的时间
+                localTime = new Date();//记录这一次点击的时间
+                if(interval<4000){//当两次进行搜索的时间不超过4秒弹出提示
+                    var dT=6;
+                    var dtInterval = setInterval(function(){
+                        dT--;
+                        if(dT<0){
+                            $('.backResult-layer').hide();
+                            $('.yys-loading').show();
+                            var reloadTime = setTimeout(function(){
+                                $('.yys-loading').hide();
+                                search();
+                                clearTimeout(reloadTime);
+                            },1000)
+                            clearInterval(dtInterval);
+                            dT = 6;
+                        }
+                        $('#yyedtime').text(dT);
+                    },1000);
+                    $('.backResult-layer').fadeIn();
+                    $(document).on('click','.backResult a',function(){
+                        $('.backResult-layer').hide();
+                        window.clearInterval(dtInterval);
+                        $('#yyedtime').text('6');//初始化返回页面的倒数总时间
+                    })
+                }else{
+                    $('.yys-loading').show();
+                        noData();
+                }
+            }else{
+                localTime = new Date();//记录第一次点击的时间
+                $('.yys-loading').show();
+                noData();
+
+            }
+        }
+
+    };
+    function noData(){
+        $.ajax({
+            type:"post",
+            url:url+"/yys/search",
+            xhrFields: { withCredentials: true },
+            data:{
+                searchType:searchType,
+                searchWord:searchWord,
+                country:country,
+                currentPage:currentPage
+            },
+            success:function(data){
+                var searchT = new Date() - searchTime;
+                var content = $('#searchInput').val().replace(/^(\s|\u00A0)+/,'').replace(/(\s|\u00A0)+$/,'');
+                $('#searchTime').html(searchT/1000);
+                $('.yys-loading').hide();
+                if(data.code===200){
+                    var list = data.data.list;
+                    $('#recordNum').html(data.data.page.totalRecord);
+                    $('.info-list-box').html('');//初始化dom中信息列表
+                    if(list.length>0){
+                        $('.noData').hide();
+                        for(var i= 0,max=list.length;i<max;i++){
+                            var desc = list[i].desc;
+                            var weburl = list[i].weburl;
+                            var subject = list[i].subject;
+                            var _id = list[i]._id;
+                            //ES6模板字符串生成搜索信息
+                            var infoList = `
+                                <div class="info-list">
+                                    <div class="info-title">
+                                        <input type="checkbox" class="mycheckbox"/>
+                                        <a href="${weburl}" target="_blank">${subject}</a>
+                                        <span class="translate-btn">
+                                            <a href="http://fanyi.baidu.com/transpage?query=${weburl}&from=auto&to=zh&source=url&render=1" target="_blank">翻译</a>
+                                        </span>
+                                    </div>
+                                    <div class="info-intro">
+                                        ${desc}
+                                    </div>
+                                    <div class="info-url">
+                                        <a href="${weburl}" target="_blank">${weburl}</a>
+                                    </div>
+                                    <div class="getInfo-box" data-id="${_id}">提取联系信息</div>
+                                </div>`
+                            infoList = infoList.replace(new RegExp(content,"igm"),"<font color='red' >"+content+"</font>");
+                            $('.info-list-box').append(infoList);
+                            //关键字变红
+                            //var myHtml = $('.info-list-box').html();//写在外面可以避免该类名的内容被改变，保证是初始加载时无关键字样式的dom
+                            //function toRed(content){
+                            //    //var myHtml = $('.info-content').html();
+                            //    var x = myHtml.replace(new RegExp(content,"igm"),"<font color='red' >"+content+"</font>");
+                            //    $('.info-list-box').html(x);
+                            //}
+                        }
+                    }else{
+                        $('.info-list-box').hide();
+                        $('.noData').show();
+                    }
+                }else{
+                    alert(data.msg)
+                }
+            }
+        })
+        //if($('#searchInput').val()===""){
+        //    $('.info-list-box').hide();
+        //    $('.noData').show();
+        //}else{
+        //    $('.info-list-box').show();
+        //    $('.noData').hide();
+        //    toRed($('#searchInput').val());
+        //}
+    };
+
+    //鹰眼一下进行搜索
+    $(document).on('click','#searchBtn',function(){
+        search()
+    });
+    $('#searchInput').on("focus",function(){
+        document.onkeydown = function(e){
+            var ev = document.all ? window.event : e;
+            if(ev.keyCode==13) {
+                search();
+            }
+        }
+    })
+});
+//其他客户购买信息滚动效果
 function autoScroll(obj){
     $(obj).find("ul").stop(false, true).animate({
         marginTop : "-50px"
@@ -106,9 +375,18 @@ function autoScroll(obj){
 $(function(){
     var scroll=setInterval('autoScroll("#FontScroll")',1500);
     $("#FontScroll").hover(function(){
-        console.log("aaa");
         clearInterval(scroll);
     },function(){
         scroll=setInterval('autoScroll("#FontScroll")',1500);
     });
 });
+
+
+$(document).on('click','#checkAll',function(){
+    if($('#checkAll').is(':checked')){
+        $('.mycheckbox').prop('checked',true)
+    }else{
+        $('.mycheckbox').prop('checked',false)
+    }
+})
+
